48de04e945d3a53c41285075f1c1b104906e2862
yahia <yahiaabdelbar@gmail.com>
2020-03-20 01:29:27 +0200

    test

--- tourism/project.pl
+++ tourism/project.pl
@@ -1,5 +1,22 @@
+
+insert(E,L,[E|L]).
+insert(E,[H|T],[H|A]):-
+	insert(E,T,A).
+	
+perm([],[]).
+perm([H|T],A):-
+	perm(T,A1),
+	insert(H,A1,A).
+	
 get_subsets([],[]).
+
 get_subsets([H|T],[H|L2]):-
-	get_subsets(T,L2).
-get_subsets([H|T],L):-
-	get_subsets(T,L).
+    get_subsets(T,L2).
+    
+get_subsets([_|T],L):-
+    get_subsets(T,L).
+	
+possibleSubset(L,A):-
+	get_subsets(L,A1),
+	perm(A1,A).
+	
