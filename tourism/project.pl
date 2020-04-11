b43625bb97d364b09738ce2990c65954df43f7dc
Ahmed hossam <toxin435@gmail.com>
2020-04-11 21:49:50 +0200

    Added recommendOffer predicate. pass

--- tourism/project.pl
+++ tourism/project.pl
@@ -1,5 +1,3 @@
-
-
 offerMean(offer(dahab, [diving, snorkeling, horseRiding], 10000, 2020-02-12, 2020-03-12,
 period(2020-03-15, 2020-04-15), 10, 5), bus).
 offerMean(offer(taba, [diving], 1000, 2020-02-12, 2020-03-12, period(2020-06-01, 2020-08-31), 10, 1),
@@ -27,7 +25,6 @@ customerPreferredAccommodation(customer(mohamed, elkasad, 1999-01-30, single, 0,
 contains([H|T],E):-
 	H = E;
 	contains(T,E).
-
 insert(E,L,[E|L]).
 insert(E,[H|T],[H|A]):-
 	insert(E,T,A).
@@ -38,11 +35,9 @@ perm([H|T],A):-
 	insert(H,A1,A).
 	
 get_subsets([],[]).
-
 get_subsets([H|T],[H|L2]):-
 	\+ H = activity(_),
     get_subsets(T,L2).
-    
 get_subsets([activity(X)|T],[activity(H1)|T1]):-
 	
 	get_subsets(X,H1),
@@ -63,9 +58,7 @@ bigger_Or_Equal_Period(A,B,C,X,Y,Z):-
     A>X;(A==X,B>Y);(A==X,B==Y,C>=Z).
 overlapPeriod(period(A-B-C,X-Y-Z),period(A2-B2-C2,X2-Y2-Z2)):-
     bigger_Or_Equal_Period(X2,Y2,Z2,A,B,C),bigger_Or_Equal_Period(X,Y,Z,A2,B2,C2).
-
 preferenceSatisfaction(_, _, [], 0).
-
 preferenceSatisfaction(Offer, Customer, [means(X)|T], S):-
 	preferenceSatisfaction(Offer, Customer, T, S1),
 	customerPreferredMean(Customer,X,R),
@@ -86,26 +79,56 @@ preferenceSatisfaction(Offer, Customer, [X|T], S):-
 	X \= accommodation(_),
 	X \= activity([_|_]),
 	preferenceSatisfaction(Offer, Customer,T, S).
-
-getOffer([],offer(_,_,_,_,_,_,_,_)).
+getOffer([],offer(D,A,C,Vf,Vt,P,Du,G)):-
+	offerMean(offer(D,A,C,Vf,Vt,P,Du,G),_).
 getOffer([activity(X)|T],offer(D,A,C,Vf,Vt,P,Du,G)):-
 	offerMean(offer(D,A,C,Vf,Vt,P,Du,G),_),
 	possibleSubset(A,X),getOffer(T,offer(D,A,C,Vf,Vt,P,Du,G)).
-
 getOffer([dest(X)|T],offer(D,A,C,Vf,Vt,P,Du,G)):-
 	offerMean(offer(D,A,C,Vf,Vt,P,Du,G),_),
 	X=D,getOffer(T,offer(D,A,C,Vf,Vt,P,Du,G)).
-
 getOffer([budget(X)|T],offer(D,A,C,Vf,Vt,P,Du,G)):-
 	offerMean(offer(D,A,C,Vf,Vt,P,Du,G), _ ) ,
 	C =< X,getOffer(T,offer(D,A,C,Vf,Vt,P,Du,G)).
-
 getOffer([period(X,Y)|T],offer(D,A,C,Vf,Vt,P,Du,G)):-
 	offerMean(offer(D,A,C,Vf,Vt,P,Du,G),_),
 	overlapPeriod(period(X,Y),P),getOffer(T,offer(D,A,C,Vf,Vt,P,Du,G)).
-
-recommendOfferForCustomer(_, [], []).
+	%recommendOfferForCustomer(_, [], []).
 recommendOfferForCustomer(Prefs, ChosenPrefs, O):-
     possibleSubset(Prefs,ChosenPrefs),
     getOffer(ChosenPrefs,O).
+getMax([],[],_,_,0).
+getMax([H1|T1],[H2|T2],Offer,N,Max):-
+	 (
+	preferenceSatisfaction(Offer, H1, H2, S),
+	N1 is N-1,
+	getMax(T1,T2,Offer,N1,NewMax1),
+	getMax(T1,T2,Offer,N,NewMax2),
+	X is S + NewMax1,
+	((X>=NewMax2 ,Max is X,N>0);((N=<0;X<NewMax2),Max is NewMax2))).
+equalsMax([],[],_,0,_,[]).
+equalsMax([H1|T1],[H2|T2],Offer,N,Max,[H1|Y]):-
+	N>0,(
+	preferenceSatisfaction(Offer, H1, H2, S),
+	N1 is N-1,
+	getMax(T1,T2,Offer,N1,NewMax1),
+	Max is S + NewMax1,
+	equalsMax(T1,T2,Offer,N1,NewMax1,Y)).
+equalsMax([H1|T1],[H2|T2],Offer,N,Max,Y):-
+	preferenceSatisfaction(Offer, H1, H2, S),
+	getMax(T1,T2,Offer,N,NewMax1),
+	Max is NewMax1,
+	equalsMax(T1,T2,Offer,N,Max,Y).	
+len([],0).
+len([_|T],L):-
+	len(T,L1),
+	L is L1 +1.
 	
+	
+recommendOffer(Customers, PreferenceList, Offer, CustomersChosen) :-
+	offerMean(Offer,_),
+	Offer = offer(_, _, _, _,_,_ ,_, N),
+	len(Customers,L),
+	((L<N,M = L) ; (L>=N,M=N)),
+	getMax(Customers,PreferenceList,Offer,M,Max),
+	equalsMax(Customers,PreferenceList,Offer,M,Max,CustomersChosen).
